2ec35d2df600ddd241dccfcf0c6b598d5c739e93
Jeremy Shaw <jeremy@seereason.com>
2010-03-29 16:57:00 +0000

    change Site to use [String], and make defaultPage a Maybe value.

--- Web/Routes/HandleT.hs
+++ Web/Routes/HandleT.hs
@@ -1,23 +1,25 @@
 module Web.Routes.HandleT where
 
+import Data.Maybe (isJust, fromJust)
 import Data.Monoid (Monoid(mempty, mappend))
+import Web.Routes.Base (decodePathInfo, encodePathInfo)
 import Web.Routes.Monad (RouteT, runRouteT)
 import Text.Parsec.Error (ParseError)
 
 data Site url a
-    = Site { handleLink  :: (url -> String) -> url -> a
-           , defaultPage :: url
-           , formatLink  :: url -> String
-           , parseLink   :: String -> Either String url
+    = Site { handleSite         :: (url -> String) -> url -> a
+           , defaultPage        :: Maybe url
+           , formatPathSegments :: url -> [String]
+           , parsePathSegments  :: [String] -> Either String url
            }
 
 withDefault :: Site url a -> String -> Either String url
 withDefault site pathInfo
-  | null pathInfo = Right (defaultPage site)
-  | otherwise     = (parseLink site) pathInfo
+  | null pathInfo && isJust (defaultPage site) = Right (fromJust (defaultPage site))
+  | otherwise     = (parsePathSegments site) (decodePathInfo pathInfo)
                          
 runSite :: String -> Site url a -> String -> (Either String a)
 runSite approot site pathInfo =
   case (withDefault site) pathInfo of
     (Left errs) -> (Left errs)
-    (Right url)  -> Right $ (handleLink site) (\url -> approot `mappend` (formatLink site url)) url
+    (Right url)  -> Right $ (handleSite site) (\url -> approot `mappend` (encodePathInfo $ formatPathSegments site url)) url
