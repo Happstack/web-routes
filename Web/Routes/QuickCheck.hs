45de6e3544295d1e7b34d514588229b20f4731c2
Jeremy Shaw <jeremy@seereason.com>
2012-07-12 22:23:31 +0000

    web-routes-th: fix new hyphenating derivePathInfo. Added derivePathInfo' that allows users to supply their own prettifier. remove Web.Routes.TH.Old

--- Web/Routes/QuickCheck.hs
+++ Web/Routes/QuickCheck.hs
@@ -3,6 +3,12 @@ module Web.Routes.QuickCheck where
 import qualified Data.Text.Encoding as Text
 import Web.Routes.PathInfo (PathInfo, toPathInfo, fromPathInfo)
 
+-- | test that a 'PathInfo' instance is valid
+--
+-- Generates 'Arbitrary' 'url' values and checks that:
+--
+--    fromPathInfo . toPathInfo == id
+--
 pathInfoInverse_prop :: (Eq url, PathInfo url) => url -> Bool
 pathInfoInverse_prop url =
     case (fromPathInfo $ Text.encodeUtf8 $ toPathInfo url) of
