23f6df1136d94c04019d03cc1aac191cc45a79c2
Jeremy Shaw <jeremy@n-heptane.com>
2015-12-18 10:18:33 -0600

    web-routes-0.27.10: allow http-types-0.9.*. Add .travis.yml and README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+web-routes [![Hackage](https://img.shields.io/hackage/v/web-routes.svg)](https://hackage.haskell.org/package/web-routes) [![Build Status](https://api.travis-ci.org/Happstack/web-routes.svg?branch=master)](https://travis-ci.org/Happstack/web-routes)
+=========
+
+A portable library which provides type-safe URL routing. For more information an tye-safe URLs check out [this section of the Happstack Book](http://www.happstack.com/docs/crashcourse/index.html#web-routes).
+
+This core library is intended to be used in conjunction with other libraries such as [web-routes-th](http://hackage.haskell.org/package/web-routes-th), [web-routes-happstack](http://hackage.haskell.org/package/web-routes-happstack), and [web-routes-boomerang](http://hackage.haskell.org/package/web-routes-boomerang).
+
+
+
+
--- shell.nix
+++ shell.nix
@@ -1,24 +1,36 @@
-with (import <nixpkgs> {}).pkgs;
-let pkg = haskellngPackages.callPackage
-            ({ mkDerivation, base, blaze-builder, bytestring, exceptions
-             , ghc-prim, http-types, HUnit, mtl, parsec, QuickCheck, split
-             , stdenv, test-framework, test-framework-hunit
-             , test-framework-quickcheck2, test-framework-th, text, utf8-string
-             }:
-             mkDerivation {
-               pname = "web-routes";
-               version = "0.27.8";
-               src = ./.;
-               buildDepends = [
-                 base blaze-builder bytestring exceptions ghc-prim http-types mtl
-                 parsec split text utf8-string
-               ];
-               testDepends = [
-                 base HUnit QuickCheck test-framework test-framework-hunit
-                 test-framework-quickcheck2 test-framework-th
-               ];
-               description = "Library for maintaining correctness and composability of URLs within an application";
-               license = stdenv.lib.licenses.bsd3;
-             }) {};
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, blaze-builder, bytestring, exceptions
+      , ghc-prim, http-types, HUnit, mtl, parsec, QuickCheck, split
+      , stdenv, test-framework, test-framework-hunit
+      , test-framework-quickcheck2, test-framework-th, text, utf8-string
+      }:
+      mkDerivation {
+        pname = "web-routes";
+        version = "0.27.9";
+        src = ./.;
+        libraryHaskellDepends = [
+          base blaze-builder bytestring exceptions ghc-prim http-types mtl
+          parsec split text utf8-string
+        ];
+        testHaskellDepends = [
+          base HUnit QuickCheck test-framework test-framework-hunit
+          test-framework-quickcheck2 test-framework-th
+        ];
+        description = "Library for maintaining correctness and composability of URLs within an application";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
 in
-  pkg.env
+
+  if pkgs.lib.inNixShell then drv.env else drv
